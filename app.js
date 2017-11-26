5879f81e359bfc88edca22b3fb5ab29034123df6
Ken Kotch <small@Kens-MacBook-Pro-2.local>
2017-11-26 09:59:04 -0700

    loads, but hangs on login screen

--- app.js
+++ app.js
@@ -377,7 +377,7 @@ export default class App extends Component {
 
     return (
       <View>
-        {this.state.ternary
+        { this.state.ternary
           ?
           <View>
             <UserInterface
